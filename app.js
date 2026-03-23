1015ef5be65be168c157b1e6e860c7bca62f98e3
Navomy20 <navomymariya20@gmail.com>
2026-03-23 22:34:36 +0530

    Completed

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-    origin: ["http://127.0.0.1:5500", "http://localhost:5500"], 
+    origin: ["http://127.0.0.1:5500", "http://localhost:5500","https://cheerful-crepe-d8c462.netlify.app"], 
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"]
